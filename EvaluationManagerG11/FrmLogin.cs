12def692c641fcf895dbbec9bdf006926b754979
Ivan Vlašić <ivlasic21@foi.hr>
2023-04-25 17:49:14 +0200

    Dorađena forma za Login

--- EvaluationManagerG11/FrmLogin.cs
+++ EvaluationManagerG11/FrmLogin.cs
@@ -9,9 +9,17 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EvaluationManagerG11 {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class frmLogin : Form {  /*klasa form1 nasljeduje klasu form*/
+        public frmLogin() {
             InitializeComponent();
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
         }
     }
 }
